65acc0945af7e868495aafba81d83491db62816c
Sameer Singh <sameer.singh@leap.com.au>
2017-05-28 12:57:10 +1000

    Add AsyncAwait.UI.Demo
    Very simple example of keeping the UI responsive while a Task on a different thread is running, and updating the UI from that thread safely.

---
+++ AsyncAwait.UI.Demo/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait.UI.Demo
+{
+    public partial class MainForm : Form
+    {
+        public int NumClicks { get; set; }
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Wrap this long-running process in a Task so that we can call it asynchronously
+        private static Task<string> UpdateUIAsync(int counter)
+        {
+            return Task.Run(async () =>
+                {
+                    await Task.Delay(1000);
+                    counter++;
+                    return string.Format("Async #{0}", counter);
+                });
+        }
+
+        // Allow this event handler to await on the asynchronous UpdateUIAsync() method
+        private async void btnClickMe_Click(object sender, EventArgs e)
+        {
+            btnClickMe.Enabled = false;
+
+            lblFeedbackAsync.Text = @"Begin async!";
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (var i = 0; i < 9; i++)
+            {
+                lblFeedback.Text = await UpdateUIAsync(i);
+            }
+
+            stopwatch.Stop();
+            var seconds = stopwatch.Elapsed.Seconds;
+            lblFeedbackAsync.Text = string.Format(@"Done async! Took {0} {1}", seconds, seconds == 1 ? "second" : "seconds");
+
+            btnClickMe.Enabled = true;
+        }
+
+        // This method synchronously updates the UI
+        private void btnMessage_Click(object sender, EventArgs e)
+        {
+            NumClicks++;
+            lblMessage.Text = string.Format("Clicked {0} {1}", NumClicks, NumClicks == 1 ? "time" : "times");
+        }
+    }
+}
---
+++ AsyncAwait.UI.Demo/MainForm.designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AsyncAwait.UI.Demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.lblFeedbackAsync = new System.Windows.Forms.Label();
+            this.btnMessage = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Location = new System.Drawing.Point(35, 50);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(61, 13);
+            this.lblFeedback.TabIndex = 0;
+            this.lblFeedback.Text = "(Feedback)";
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(35, 132);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(75, 23);
+            this.btnClickMe.TabIndex = 1;
+            this.btnClickMe.Text = "Click Me";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // lblFeedbackAsync
+            // 
+            this.lblFeedbackAsync.AutoSize = true;
+            this.lblFeedbackAsync.Location = new System.Drawing.Point(35, 91);
+            this.lblFeedbackAsync.Name = "lblFeedbackAsync";
+            this.lblFeedbackAsync.Size = new System.Drawing.Size(93, 13);
+            this.lblFeedbackAsync.TabIndex = 2;
+            this.lblFeedbackAsync.Text = "(Feedback Async)";
+            // 
+            // btnMessage
+            // 
+            this.btnMessage.Location = new System.Drawing.Point(218, 132);
+            this.btnMessage.Name = "btnMessage";
+            this.btnMessage.Size = new System.Drawing.Size(90, 23);
+            this.btnMessage.TabIndex = 3;
+            this.btnMessage.Text = "Show Message";
+            this.btnMessage.UseVisualStyleBackColor = true;
+            this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(215, 50);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(56, 13);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "(Message)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 195);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnMessage);
+            this.Controls.Add(this.lblFeedbackAsync);
+            this.Controls.Add(this.btnClickMe);
+            this.Controls.Add(this.lblFeedback);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task-based Async/Await Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Label lblFeedbackAsync;
+        private System.Windows.Forms.Button btnMessage;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
